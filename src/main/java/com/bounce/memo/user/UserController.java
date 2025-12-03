ab8b0bf15da712a01ba88a492bcd9ba97e6d2fe3
bouncewater <bouncewater48@gmail.com>
2025-12-03 18:37:44 +0900

    [강승훈] - user/join.html 생성

---
+++ src/main/java/com/bounce/memo/user/UserController.java
@@ -0,0 +1,17 @@
+package com.bounce.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+
+    @GetMapping("/join")
+    public String join() {
+        return "user/join";
+    }
+
+}
